d85ee5419d5775a9b502843e5460084b93e1d817
Abdulrazak Auwal <razakauwal@Gmail.com>
2020-07-24 01:41:15 +0100

    Updates

---
+++ precache-manifest.29bea282f87102e229e807643bc5928e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7fc1f75b10aa9cf8a26aff3add4fc8f7",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "ecfa341a79fe374ca7c5",
+    "url": "/robofriends/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "5c9e325ad85f4a63be93",
+    "url": "/robofriends/static/css/main.8f763cf6.chunk.css"
+  },
+  {
+    "revision": "ecfa341a79fe374ca7c5",
+    "url": "/robofriends/static/js/2.b056fbe4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robofriends/static/js/2.b056fbe4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5c9e325ad85f4a63be93",
+    "url": "/robofriends/static/js/main.c29bea79.chunk.js"
+  },
+  {
+    "revision": "78ad030deeb63f6ee6c2",
+    "url": "/robofriends/static/js/runtime-main.fa4e25fb.js"
+  }
+]);
